f5c5af02b68b9a5c533f0b8d33a47de76e9187f7
Haicheng Li <haicheng.li@outlook.com>
2022-10-29 06:24:41 +0800

    intr: add cpu run level switch support
    but still buggy that supervisor trap handler still cannot be triggered
    and user mode can only run one instruction.
    
    Signed-off-by: Haicheng Li <haicheng.li@outlook.com>

--- intr.s
+++ intr.s
@@ -1,6 +1,6 @@
 	.text
-	.globl init_vector
 
+	.globl init_vector
 init_vector:
 	la a0, vect_table
 	addi a0, a0, 1
@@ -9,6 +9,86 @@ init_vector:
 	csrw mie, a0
 	csrw mstatus, a0
 	ret
+
+	.globl init_trap
+init_trap:
+	addi sp, sp, -8
+	sd ra, 0(sp)
+	la a0, trap_msg
+	call printk
+
+	li t0, 0x222
+	csrs mideleg, t0
+	li t0, 0
+	csrw mie, t0
+
+	la t0, mtrap_handler
+	csrw mtvec, t0
+	la t0, strap_handler
+	csrw stvec, t0
+
+	li t0, 0x1800
+	csrc mstatus, t0
+	li t0, 0x800
+	csrs mstatus, t0
+	li t0, 0x100
+	csrw medeleg, t0
+
+	ld ra, 0(sp)
+	addi sp, sp, 8
+	ret
+
+	.globl switch_ctx
+switch_ctx:
+	la a0, switch_msg
+	call printk
+	la t0, kernel
+	csrw mepc, t0
+	mret
+
+
+mtrap_handler:
+	la a0, mtrap_msg
+	call printk
+	la t0, kernel
+	csrw mepc, t0
+	mret
+
+kernel:
+	la a0, kernel_msg
+	call printk
+	la t0, user3
+	csrw sepc, t0
+	sret
+
+strap_handler:
+	la a0, strap_msg
+	call printk
+	ecall
+
+user3:
+	la a0, user3_msg
+	call printk
+	ecall
+
+trap_msg:
+	.string "init_trap\n"
+
+user3_msg:
+	.string "i'm app\n"
+
+kernel_msg:
+	.string "i'm kernel\n"
+
+switch_msg:
+	.string "switch context\n"
+
+mtrap_msg:
+	.string "mtrap handler\n"
+
+strap_msg:
+	.string "strap handler\n"
+
 
 	.balign 256
 vect_table:
@@ -36,3 +116,12 @@ vect_rsv10:
 	j expt_handler
 vect_mexti:
 	j mexti_handler
+
+
+	.balign 256
+vect_mtrap:
+	j mtrap_handler
+
+	.balign 256
+vect_strap:
+	j strap_handler
