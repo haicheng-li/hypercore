d76319cd7cf45663a7dfd4773cfb204e5ae411fa
Haicheng Li <haicheng.li@outlook.com>
2022-11-24 22:25:34 +0800

    start: cleanup useless code
    Signed-off-by: Haicheng Li <haicheng.li@outlook.com>

--- start.s
+++ start.s
@@ -2,22 +2,12 @@
 
 	.global _start
 _start:
-	#j start
-	li a3, 0
-	mv ra, a0
-	mv a0, a3
-	add a1, a0, 100
-	lw a2, message
-	mv a0, a2
-	mv a1, a2
 	jal init_stack
 #	call init_vector
 	#call test_swi
 	call init_trap
 	#call switch_mode
-	#call setup_mmu
 	call switch_ctx
-	mv ra, a3
 	la ra, _start
 	ret	
 
